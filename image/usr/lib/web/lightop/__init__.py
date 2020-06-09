68e7b1d6f773fef91463354ee8102d72b0bee3ef
pgt <philipp.tenbrock@ipa.fraunhofer.de>
2020-06-09 18:59:33 +0200

    close VNC again after having no VNC connection for 15 sec

--- image/usr/lib/web/lightop/__init__.py
+++ image/usr/lib/web/lightop/__init__.py
@@ -3,6 +3,8 @@ from flask import (Flask,
                    abort,
                    )
 import os
+import psutil
+import threading
 
 
 # Flask app
@@ -27,7 +29,53 @@ import subprocess
 import time
 
 
-FIRST = True
+RUNNING = False
+KILL_VNC_AFTER = 15
+NO_VNC_CONN_FOR = 0
+
+
+def start_vnc_check_timer():
+    global NO_VNC_CONN_FOR
+    NO_VNC_CONN_FOR = 0
+    vnc_check_timer()
+
+
+def vnc_check_timer():
+    global RUNNING, KILL_VNC_AFTER, NO_VNC_CONN_FOR
+
+    if not RUNNING:
+        return
+
+    if NO_VNC_CONN_FOR >= KILL_VNC_AFTER:
+        # stop VNC
+        subprocess.check_call(r"supervisorctl stop all", shell=True)
+
+        # check all running
+        for i in xrange(20):
+            output = subprocess.check_output(r"supervisorctl status | grep RUNNING | wc -l", shell=True)
+            if output.strip() == "0":
+                RUNNING = False
+                return
+            time.sleep(2)
+        abort(500, 'service is not ready, please restart container')
+    else:
+        # Check if there is at least one vnc connection
+        x11vnc_pid = [proc.pid for proc in psutil.process_iter() if proc.name() == "x11vnc"]
+        if len(x11vnc_pid) != 1:
+            NO_VNC_CONN_FOR += 1
+        else:
+            x11vnc_pid = x11vnc_pid[0]
+            connections = psutil.net_connections()
+            conns = [conn for conn in connections if conn.status == "ESTABLISHED" and conn.pid == x11vnc_pid]
+            if len(conns) == 0:
+                NO_VNC_CONN_FOR += 1
+            else:
+                NO_VNC_CONN_FOR = 0
+
+        t = threading.Timer(1.0, vnc_check_timer)
+        t.daemon = True
+        t.start()
+
 
 
 def exception_to_json(func):
@@ -87,14 +135,16 @@ HTML_REDIRECT = '''<html><head>
 
 @app.route('/')
 def index():
+    if RUNNING:
+        return HTML_REDIRECT
     return HTML_INDEX
 
 
 @app.route('/redirect.html')
 def redirectme():
-    global FIRST
+    global RUNNING
 
-    if not FIRST:
+    if RUNNING:
         return HTML_REDIRECT
 
     env = {'width': 1024, 'height': 768}
@@ -114,8 +164,9 @@ def redirectme():
     for i in xrange(20):
         output = subprocess.check_output(r"supervisorctl status | grep RUNNING | wc -l", shell=True)
         if output.strip() == "6":
-            FIRST = False
+            RUNNING = True
             time.sleep(5) # make sure VNC is actually running before redirecting
+            start_vnc_check_timer()
             return HTML_REDIRECT
         time.sleep(2)
     abort(500, 'service is not ready, please restart container')
